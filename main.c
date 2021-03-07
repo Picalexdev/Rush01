fb83fefc3551fdc4e830e76e8a0ce84f10b5cb22
Cristobal Del pino millar <cdel-pin@c3r6s3.42madrid.com>
2021-03-07 18:47:16 +0100

    main normi

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: apico-su <apico-su@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 09:13:48 by apico-su          #+#    #+#             */
-/*   Updated: 2021/03/07 16:32:27 by apico-su         ###   ########.fr       */
+/*   Updated: 2021/03/07 18:43:40 by cdel-pin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,21 @@ void write_output_table(char ***output);
 char ***create_output_table();
 void num_checker(char ***output, char **input);
 
-
-int     main(int count, char **values)
+int		main(int count, char **values)
 {
-    char **input_table;
-    char ***output_table;
+	char **input_table;
+	char ***output_table;
 
-    input_table = input_numbers(values);
-    if (input_table[0][0] == '0' || count < 2)
-    {
-        write(1, "Error\n", 6);
-        exit(0);
-    }
-    write_input_table(input_table);
-    output_table = create_output_table();
-    num_checker(output_table, input_table);
-    write_output_table(output_table);
-
-    return 0;
+	input_table = input_numbers(values);
+	if (input_table[0][0] == '0' || count < 2)
+	{
+		write(1, "Error\n", 6);
+		exit(0);
+	}
+	write_input_table(input_table);
+	output_table = create_output_table();
+	num_checker(output_table, input_table);
+	write_output_table(output_table);
+	return (0);
 }
+
